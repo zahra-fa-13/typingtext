64666a5bff52ec7f41d2ba6cf31855a15ce875fe
Zahra <166248394+zahra-fa-13@users.noreply.github.com>
2025-02-09 16:11:57 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1
+var typed = new Typed("#typed-text", {
+    strings: ["لورم یک متن ساختگی است"], 
+    typeSpeed: 100,  // سرعت تایپ (میلی‌ثانیه)
+    loop: false   
+});
+
+// 2
+// let text = "آیا لورم یک متن ساختگی است";
+// let index = 0;
+// let speed = 100; // سرعت تایپ
+
+// function typeWriter() {
+//     if (index < text.length) {
+//         document.getElementById("text").innerHTML += text.charAt(index);
+//         index++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+
+// typeWriter(); // اجرا شدن تایپ متن
